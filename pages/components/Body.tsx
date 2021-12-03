63fa366ba18544cc5b2fb7b534a430707470600c
magic-kiri <kimuapon@gmail.com>
2021-12-04 01:48:48 +0600

    import loader

--- pages/components/Body.tsx
+++ pages/components/Body.tsx
@@ -5,52 +5,14 @@ import RunningExamCard from "./RunningExamCard";
 import ExamCard from "./ExamCard";
 
 const Body = () => {
+  
+
   return (
     <div className={bodystyle.bodyrest}>
       <RunningExamCard
         title="Database Management System and Software Engineering"
         courseCode="CSE 334"
         time="01:29:45"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
-      />
-      <ExamCard
-        title="Database Management System and Confused Unga Bunga with Many More Shits"
-        courseCode="CSE 333"
-        time="2:30PM, 29 October 2019"
       />
       <ExamCard
         title="Database Management System and Confused Unga Bunga with Many More Shits"
