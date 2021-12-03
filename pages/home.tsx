e65bcad1807419bf8e113894e95628968f1e5fb2
Bishal <arbishal.319@gmail.com>
2021-12-04 03:35:01 +0600

    Fix loading

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,24 +4,45 @@ import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import { UserContext } from "./_app";
 import Loading from "../src/ui-custom-components/Loading";
+import Title from "../src/ui-custom-components/Title";
 
 function Home() {
   const [session, loading] = useSession();
-  return (
-    <div>
-      {loading ? (
-        <>
-          <Navbar />
-          <Loading />
-        </>
-      ) : (
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "200px",
+        }}
+      >
+        <Loading />
+      </div>
+    );
+  } else {
+    if (session) {
+      return (
         <>
           <Navbar />
           <Body />
         </>
-      )}
-    </div>
-  );
+      );
+    }
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "200px",
+        }}
+      >
+        <Title style={{ fontSize: "24px" }}>You are not logged in.</Title>
+      </div>
+    );
+  }
 }
 
 export default Home;
