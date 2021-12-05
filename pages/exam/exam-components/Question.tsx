0af9f23ea6d38028c039825e497f2ece6f20d589
Bishal <arbishal.319@gmail.com>
2021-12-05 13:27:17 +0600

    Add dummy text and link in questions

--- pages/exam/exam-components/Question.tsx
+++ pages/exam/exam-components/Question.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Upload from "../../../src/ui-custom-components/Upload";
+import Text from "../../../src/ui-custom-components/Text";
+import Link from "../../../src/ui-custom-components/Link";
 
 const Question = () => {
-  return <Upload/>;
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Text style={{ fontSize: "18px" }}> Exam Code: </Text>
+      <Link href="https://google.com" target="_blank">
+        Link goes here
+      </Link>
+      <Upload />
+    </div>
+  );
 };
 
 export default Question;
