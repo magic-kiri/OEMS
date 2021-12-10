a50c2395db88dc80fe7ef269f4d60fe70372b936
Bishal <arbishal.319@gmail.com>
2021-12-10 21:50:50 +0600

    Add download script modal

--- pages/exam/exam-components/QuestionTeacher.tsx
+++ pages/exam/exam-components/QuestionTeacher.tsx
@@ -4,10 +4,16 @@ import Text from "../../../src/ui-custom-components/Text";
 import Link from "../../../src/ui-custom-components/Link";
 import Button from "../../../src/ui-custom-components/Button";
 import QuestionStyle from "./question.module.css";
+import DownloadScriptModal from "./DownloadScriptModal";
 
 const QuestionTeacher = () => {
   const [status, setStatus] = useState<string>("finished");
   const [link, setLink] = useState<string>();
+  const [openDownloadScript, setOpenDownloadScript] = useState<boolean>(false);
+  const handleChange = () => {
+    setOpenDownloadScript((value) => !value);
+  };
+
   if (status === "upcoming") {
     return (
       <div className={QuestionStyle.question}>
@@ -49,9 +55,17 @@ const QuestionTeacher = () => {
             Question link.
           </Link>
         )}
-        <Button theme="dark" style={{ marginTop: "10px" }}>
+        <Button
+          theme="dark"
+          style={{ marginTop: "10px" }}
+          onClick={handleChange}
+        >
           Download Scripts
         </Button>
+        <DownloadScriptModal
+          open={openDownloadScript}
+          setOpen={setOpenDownloadScript}
+        />
       </div>
     );
   }
